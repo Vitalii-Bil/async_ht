b4ebac16e6d1b69a64538578c9ae41ec23cc600f
Your Name <you@example.com>
2021-04-02 15:41:06 +0300

    Retake

--- weather.py
+++ weather.py
@@ -1,29 +1,51 @@
 import asyncio
 import requests
 import os
+from aiohttp import ClientSession
 
-async def main():
+openweathermap_api_key = os.environ.get("openweathermap_api_key")
+weatherbit_api_key = os.environ.get("weatherbit_api_key")
+
+url1 = 'https://www.metaweather.com/api/location/44418/'
+url2 = f'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID={openweathermap_api_key}'
+url3 = f'https://api.weatherbit.io/v2.0/current?lat=51.509865&lon=-0.118092&key={weatherbit_api_key}&include=minutely'
+
+
+async def get_weather(url, session):
+    try:
+        response = await session.request(method='GET', url=url)
+    except requests.exceptions.RequestException as e:
+        print ("Error: ", e)
+    response = await response.json()
+    return response
+
+
+async def metaweather(url, session):
+    response = await get_weather_details_async(url, session)
+    return float(response['consolidated_weather'][0]['the_temp'])
+
+
+async def openweathermap(url, session):
+    response = await get_weather_details_async(url, session)
+    return float(response['main']['temp'])
+
+
+async def weatherbit(url, session):
+    response = await get_weather_details_async(url, session)
+    return loat(response['data'][1]['temp'])
+
+
+async def run_program():
+    async with ClientSession() as session:
+        result = await asyncio.gather(metaweather(url1, session),
+                                      openweathermap(url2, session),
+                                      weatherbit(url3, session))
+        
+        print(f'Temp in London = {(result[0] + result[1] + result[2])/3}')
+
+
+def main():
     '''This application collect current London temp from 3 API services and then compare them.'''
-    openweathermap_api_key = os.environ.get("openweathermap_api_key")
-    weatherbit_api_key = os.environ.get("weatherbit_api_key")
-
-    url1 = 'https://www.metaweather.com/api/location/44418/'
-    url2 = f'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID={openweathermap_api_key}'
-    url3 = f'https://api.weatherbit.io/v2.0/current?lat=51.509865&lon=-0.118092&key={weatherbit_api_key}&include=minutely'
-
     loop = asyncio.get_event_loop()
-    future1 = loop.run_in_executor(None, requests.get, url1)
-    future2 = loop.run_in_executor(None, requests.get, url2)
-    future3 = loop.run_in_executor(None, requests.get, url3)
-    response1 = await future1
-    response2 = await future2
-    response3 = await future3
-    temp1 = float(response1.json()['consolidated_weather'][0]['the_temp'])
-    temp2 = float(response2.json()['main']['temp'])
-    temp3 = float(response3.json()['data'][1]['temp'])
-    av_temp = (temp1 + temp2 + temp3) / 3
-
-    print(f'Temp in London = {av_temp}')
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+    loop.run_until_complete(run_program())
+    loop.close()
